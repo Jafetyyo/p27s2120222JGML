db08b2bf85b906b7de9037af12c4843aea7f997d
ISC Tesoem <tesoem@iMac-Tesoem-3.local>
2022-09-28 15:18:05 -0500

    Initial commit

---
+++ app/src/test/java/mx/edu/tesoem/ics/p2jjgt/p27s2120222jgml/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package mx.edu.tesoem.ics.p2jjgt.p27s2120222jgml;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
